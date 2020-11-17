55c2504b05c9c0ece499e22511e7a8db2feaab0a
Oscar Lucas <olucas@espol.edu.ec>
2020-11-16 20:52:05 -0500

    ArrayList Methods implemented: every add and auxiliary methos as isFull() and increaseCapacity()

--- The-problem-of-Flavio-Josefo/src/DataStructure/List.java
+++ The-problem-of-Flavio-Josefo/src/DataStructure/List.java
@@ -4,7 +4,30 @@ package DataStructure;
  *
  * @author oscarlucas7
  */
-public interface List {
+public interface List<E> {
+    
+    public E addFirst(E element);
+    
+    public E addLast(E element);
+    
+    public E add(int index, E element);
+    
+    public E removeFirst(E element);
+    
+    public E removeLast(E element);
+    
+    public E remove(int index, E element);
+    
+    public E get(int index);
+    
+    public E set(int index, E element);
+    
+    public void clear();
+    
+    public boolean isEmpty();
+    
+    public int size();
+    
     
     
 }
